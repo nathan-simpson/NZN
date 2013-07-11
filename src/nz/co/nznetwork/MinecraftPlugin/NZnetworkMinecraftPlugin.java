138d01ef16031d1d9e22fe9e89761d0cfea1928a
RangaHax <andrewdavies007@gmail.com>
2013-07-11 20:58:04 +1200

    Eclipse test

--- src/nz/co/nznetwork/MinecraftPlugin/NZnetworkMinecraftPlugin.java
+++ src/nz/co/nznetwork/MinecraftPlugin/NZnetworkMinecraftPlugin.java
@@ -2,6 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  * test edit by andrew
+ * test edit from andrew from eclipse
  */
 package nz.co.nznetwork.MinecraftPlugin;
 
